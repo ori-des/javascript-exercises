e11b4279d729a507f1087c337ec124c2cebf1785
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-13 16:39:29 +0100

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+    let argArr = Array.from(args);
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.indexOf(arr[i]) !== arr.indexOf(argArr[0]) && arr.indexOf(arr[i]) !== arr.indexOf(argArr[1]) && arr.indexOf(arr[i]) !== arr.indexOf(argArr[2]) && arr.indexOf(arr[i]) !== arr.indexOf(argArr[3])) {
+            newArr.push(arr[i]);
+        };
+    } return newArr;
 };
 
 // Do not edit below this line

d8552707452055e84f9fd7f1e3b16e002db7fc5f
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-13 18:32:46 +0100

    Solve temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = 5/9 * (fahrenheit - 32)
+  return +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line

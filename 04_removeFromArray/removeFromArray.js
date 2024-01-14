a0215bc36e28bcbccc9cfa55f5f3cc52d3ab7095
tensaw <114301480+tensaw@users.noreply.github.com>
2024-01-13 19:41:55 -0600

    Simplify if-statement
    I think this is the best way to express what we want to happen in this function. Instead of checking to see if each value in `args` is also in `arr` (whether through directly accessing them, as you did, or through a nested loop), we instead flip this around and ask, "Does the current value of `arr` have an index in `args`?" If it does, we skip, and if it doesn't, we push it.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function (arr, ...args) {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
-        if (arr.indexOf(args[0]) === -1 && arr.indexOf(args[1]) === -1 && arr.indexOf(args[2]) === -1 && arr.indexOf(args[3]) === -1) {
+        if (args.indexOf(arr[i] === -1) {
             newArr.push(arr[i]);
         };
     } return newArr;

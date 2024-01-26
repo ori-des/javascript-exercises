96b894bafe858ee4908d5ca7164b4f2a859a91b0
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-26 16:10:33 +0100

    Solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleaned = string.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    let reversed = string.replace(/[^a-zA-Z0-9]/g,'').split("").reverse().join("").toLowerCase();
+    console.log(cleaned);
+    console.log(reversed);
+    if (cleaned === reversed) {
+        return true;
+    } return false;
 };
 
 // Do not edit below this line

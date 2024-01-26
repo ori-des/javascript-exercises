d780794de1aaee409f4ff02f6d63c0ef212d1251
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-26 15:52:36 +0100

    Solved calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  } return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  for (let i = 0; i < arr.length; i++) {
+    sum *= arr[i];
+  } return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  } return factorial;
 };
 
 // Do not edit below this line

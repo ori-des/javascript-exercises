1293493b01b4ad8a53f75a807809d8d3f1a44441
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-26 16:30:14 +0100

    Solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    } if (num == 0) {
+        return 0;
+    } if (typeof num === 'string') {
+        num = +num;
+    }
+    let fibonacci = [1, 1];
+    for (let i = 1; i < num; i++) {
+        let nextNum = 0;
+        nextNum = fibonacci[i] + fibonacci[i - 1];
+        fibonacci.push(nextNum);
+    } return fibonacci[num - 1];
 };
 
 // Do not edit below this line

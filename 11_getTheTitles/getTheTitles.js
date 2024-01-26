15ba39be3c655ecfca85ba577d1e29030385697c
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-26 17:05:25 +0100

    Solved getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let firstObj = array[0];
+    let secondObj = array[1];
+    let firstValue = [];
+    let secondValue = [];
+    for (let value in firstObj) {
+        firstValue.push(firstObj[value]);
+    } 
+    for (let value in secondObj) {
+        secondValue.push(secondObj[value]);
+    } 
+    return [firstValue[0], secondValue[0]];
 };
 
 // Do not edit below this line

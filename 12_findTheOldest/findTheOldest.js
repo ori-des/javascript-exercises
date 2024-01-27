eca70453ed796c72d63acb2285d81a88112167d4
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-27 12:14:01 +0100

    Solved one test of find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    let oldestPerson = {};
+    let prevAge = 0;
+    for (let i = 0; i < array.length; i++) {
+        let yearOfBirth = array[i]['yearOfBirth'];
+        let yearOfDeath = array[i]['yearOfDeath'];
+        let age = yearOfDeath - yearOfBirth;
+        if (age > prevAge) {
+            prevAge = age;
+            oldestPerson = array[i];
+        }
+    } return oldestPerson;
 };
 
 // Do not edit below this line
